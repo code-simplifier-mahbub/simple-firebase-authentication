a28443f20e86fb81ab3dcf708d61bbaa7b458bc3
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-10-24 20:28:16 +0600

    [DONE] google and github sing in method complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import "./App.css";
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "./firebase/firebase.init";
+import { useState } from "react";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 function App() {
- const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+  const GoogleProvider = new GoogleAuthProvider();
+  const GithubProvider = new GithubAuthProvider();
 
   const handleGoogleSign = () => {
-    signInWithPopup(auth, provider)
-    .then(result => {
-      const user = result.user;
+    signInWithPopup(auth, GoogleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error("error : ", error);
+      });
+  };
+  const handleGithubSingIn = ()=> {
+    signInWithPopup(auth, GithubProvider)
+    .then((result) => {
+      const user = result.user
+      setUser(user)
       console.log(user)
-    }).catch(error => {
-      console.error('error : ', error)
-    })
+    }).catch((error => {
+      console.error('error', error)
+    }))
+  }
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({});
+      });
   };
   return (
     <div className="App">
-      <button onClick={handleGoogleSign}>Sign in with Google</button>
+      {user.uid ? (
+        <button onClick={handleGoogleSignOut}>Sign Out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSign}>Google SignIn </button>
+          <button onClick={handleGithubSingIn}>Github SignIn</button>
+        </>
+      )}
+      {user.uid && (
+        <div>
+          <h1>Name: {user.displayName}</h1>
+          <p>Email : {user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 }
